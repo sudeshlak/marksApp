22d4a48d375164614b2f1af6019cd22f60ab71d7
sudeshlak <swelikotuwa@gmail.com>
2021-06-02 09:01:13 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function App() {
+  return <></>
+}
+
+export default App;
---
+++ src/views/Marks.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Marks: React.FC = () => {
+    return <div></div>
+}
+export default Marks;
