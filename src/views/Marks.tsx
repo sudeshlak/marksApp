7e73197b6e47c726e8c67630f0fa2db7b9e94c81
sudeshlak <swelikotuwa@gmail.com>
2021-06-04 05:33:12 +0530

    dark mode,sort and history

--- src/views/Marks.tsx
+++ src/views/Marks.tsx
@@ -1,87 +1,81 @@
 import React, {useState} from 'react';
-import {Col, Container, Row} from "react-bootstrap";
-import Team from "../components/Team";
+import {Container, Row} from "react-bootstrap";
 import AddTeam from "../components/AddTeam";
 import Theme from "../components/Theme";
+import {ITeam} from "../types/MarksTypes";
+import Teams from "../components/Teams";
 
 const Marks: React.FC = () => {
-    const t: { id: number, name: string, marks: number }[] = [{id: 1, name: 'team1', marks: 100}, {
+    const t: ITeam[] = [{id: 1, name: 'team1', marks: [{date: '2021-5-5', mark: 100}]}, {
         id: 2,
         name: 'team2',
-        marks: 200
-    }, {id: 3, name: 'team3', marks: 300}, {id: 4, name: 'team4', marks: 300}]
-    const [teams, setTeams] = useState<{ id: number, name: string, marks: number }[]>(t);
+        marks: [{date: '2021-5-5', mark: 100}]
+    }, {id: 3, name: 'team3', marks: [{date: '2021-5-5', mark: 100}]}, {
+        id: 4,
+        name: 'team4',
+        marks: [{date: '2021-5-5', mark: 100}]
+    }];
+    const [teams, setTeams] = useState<ITeam[]>(t);
+    const [isDark, setIsDark] = useState<boolean>(false);
+    const [isSort, setIsSort] = useState<boolean>(false);
 
 
-    /*const handleOnDelete=()=>{
-
-    }*/
-    const handleOnAddMarks=(id:number,addMarks:number)=>{
-        const allTeams: { id: number, name: string, marks: number }[] = teams.slice();
-        setTeams(
-            allTeams.map((team) =>
-                team.id === id ? {...team, marks: team.marks+addMarks} : team
-            )
-        );
-
+    const handleOnDark=(x:boolean)=>{
+        setIsDark(!x);
     }
-    const handleOnSubMarks=(id:number,addMarks:number)=> {
-        const allTeams: { id: number, name: string, marks: number }[] = teams.slice();
-        setTeams(
-            allTeams.map((team) =>
-                team.id === id ? {...team, marks: team.marks-addMarks} : team
-            )
-        );
+    const handleOnSort=(x:boolean)=>{
+        console.log(x);
+        setIsSort(!x);
     }
-    const generateId=()=>{
-        return Math.floor(Math.random()*10000)+1
+    const generateId = () => {
+        return Math.floor(Math.random() * 10000) + 1
     }
-    const handleOnTeamAdd=(name:string)=>{
-        const allTeams: { id: number, name: string, marks: number }[] = teams.slice();
+    const getDate = () => {
+        const date: string = String(new Date());
+        return date;
+    }
+
+    const handleOnTeamAdd = (name: string) => {
+        const allTeams: ITeam[] = teams.slice();
         const ids: number[] = teams.map(team => team.id);
-        let id:number=1;
+        let id: number = 1;
         while (ids.includes(id)) {
-            id=generateId();
+            id = generateId();
         }
-        const newTeam: { id: number, name: string, marks: number } = {id: id, name: name, marks: 0};
+        const newTeam: ITeam = {id: id, name: name, marks: [{date: getDate(), mark: 0}]};
         allTeams.push(newTeam);
         setTeams(allTeams);
-
     }
 
-    const renderTeam = () => {
-        if (!teams) {
-            return;
-        }
-        return teams.map(
-            (team) => {
-                return <Team team={team}
-                             key={team.id}
-                             onAddMarks={handleOnAddMarks}
-                             onSubMarks={handleOnSubMarks}
-                    /*onDelete={}
-                    onAddMarks={}
-                    onSubMarks={}
-                    viewHistory={}*/
-                />
-            }
-        );
-    }
+
+
+
     return (
         <Container className='marks-app'>
             <Row className='title text-center py-2'>
                 <h1>MARKS</h1>
             </Row>
             <Row>
-                <AddTeam onTeamAdd={handleOnTeamAdd}/>
-            </Row>
-            <Row>
-                <Theme/>
-            </Row>
+                <AddTeam onTeamAdd={handleOnTeamAdd}
+                         isDark={isDark}
 
-            <Row>
-                {renderTeam()}
+                />
             </Row>
+            <Row>
+                <Theme isDark={isDark}
+                       isSort={isSort}
+                       onDark={handleOnDark}
+                       onSort={handleOnSort}
+                />
+            </Row>
+            <Teams isDark={isDark}
+                   onToggle={handleOnDark}
+                   teams={teams}
+                   setTeams={setTeams}
+                   isSort={isSort}
+            />
+
+
         </Container>);
 }
 export default Marks;
